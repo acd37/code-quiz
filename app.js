d3ac4b4ece24443b37d0c15052cffb4fe66f6842
Alec Down <alecdown@gmail.com>
2020-02-18 22:46:38 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,111 @@
+let gameTimer;
+const numOfQuestions = questions.length;
+let time = 60;
+let currentQuestion = 0;
+let correctAnswers = 0;
+let wrongAnswers = 0;
+
+function initGame() {
+    correctAnswers = 0;
+    wrongAnswers = 0;
+    currentQuestion = 0;
+
+    document.getElementById('start').classList.add('hidden');
+    document.getElementById('quiz_row').classList.remove('hidden');
+    generateQuestion();
+
+    gameTimer = setInterval(function() {
+        time--;
+        if (time <= 0) {
+            document.getElementById('time').innerHTML = 'Game over!';
+            endGame();
+        } else {
+            document.getElementById('time').innerHTML = time;
+        }
+    }, 1000);
+}
+
+function generateQuestion() {
+    if (currentQuestion === numOfQuestions) {
+        endGame();
+    } else {
+        document.getElementById('answers').innerHTML = '';
+
+        document.getElementById('question').innerHTML =
+            questions[currentQuestion].question;
+
+        for (
+            let i = 0;
+            i < questions[currentQuestion].potential_answers.length;
+            i++
+        ) {
+            const el = document.createElement('p');
+            el.innerHTML =
+                questions[currentQuestion].potential_answers[i].answer;
+            el.setAttribute(
+                'data-answer-id',
+                questions[currentQuestion].potential_answers[i].id
+            );
+            el.addEventListener('click', checkAnswer);
+            document.getElementById('answers').appendChild(el);
+        }
+    }
+}
+
+function endGame() {
+    clearInterval(gameTimer);
+    document.getElementById('quiz_row').classList.add('hidden');
+
+    const resultText = document.createElement('p');
+    resultText.innerHTML = 'Quiz Complete!';
+
+    const resultScore = document.createElement('p');
+    resultScore.innerHTML =
+        'Correct answers: ' + correctAnswers + '/' + questions.length;
+
+    document.getElementById('announcement').appendChild(resultText);
+    document.getElementById('announcement').appendChild(resultScore);
+}
+
+function checkAnswer() {
+    const selectedAnswer = this.getAttribute('data-answer-id');
+    console.log('sel', selectedAnswer);
+    console.log('ans', questions[currentQuestion].correct_answer);
+
+    if (selectedAnswer === questions[currentQuestion].correct_answer) {
+        correctAnswers++;
+
+        document
+            .querySelector(`[data-answer-id*="${selectedAnswer}"]`)
+            .classList.add('correct');
+
+        document.getElementById('correct_answers').innerHTML = correctAnswers;
+        document.getElementById('announcement').innerHTML =
+            'Correct!! +5 seconds';
+
+        time = time + 10;
+    } else {
+        wrongAnswers++;
+
+        document
+            .querySelector(`[data-answer-id*="${selectedAnswer}"]`)
+            .classList.add('wrong');
+
+        document.getElementById('wrong_answers').innerHTML = wrongAnswers;
+        document.getElementById('announcement').innerHTML =
+            'Wrong!! -10 seconds';
+        time = time - 10;
+        console.log('incorrect');
+    }
+
+    setTimeout(function() {
+        currentQuestion++;
+        generateQuestion();
+    }, 1500);
+}
+
+function writePassword() {
+    console.log('write');
+}
+
+document.getElementById('start').addEventListener('click', initGame);
---
+++ questions.js
@@ -0,0 +1,222 @@
+const questions = [
+    {
+        question: 'What is my middle name?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'Charles'
+            },
+            {
+                id: 'b',
+                answer: 'Frederick'
+            },
+            {
+                id: 'c',
+                answer: 'Allen'
+            },
+            {
+                id: 'd',
+                answer: "I don't have one."
+            }
+        ],
+        correct_answer: 'a'
+    },
+    {
+        question: 'Where did I first attend graduate school?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'The University of Utah'
+            },
+            {
+                id: 'b',
+                answer: 'Duke University'
+            },
+            {
+                id: 'c',
+                answer: 'Vanderbilt University'
+            },
+            {
+                id: 'd',
+                answer: 'The Penn State University'
+            }
+        ],
+        correct_answer: 'b'
+    },
+    {
+        question: 'What is my favourite programming framework/library?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'VueJS'
+            },
+            {
+                id: 'b',
+                answer: 'Angular'
+            },
+            {
+                id: 'c',
+                answer: 'ReactJS'
+            },
+            {
+                id: 'd',
+                answer: 'jQuery'
+            }
+        ],
+        correct_answer: 'c'
+    },
+    {
+        question: 'What is my preferred spirit?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'Vodka'
+            },
+            {
+                id: 'b',
+                answer: 'Tequila'
+            },
+            {
+                id: 'c',
+                answer: 'Rum'
+            },
+            {
+                id: 'd',
+                answer: 'Whiskey'
+            }
+        ],
+        correct_answer: 'a'
+    },
+    {
+        question: 'Which languages other than English do I speak?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'German & French'
+            },
+            {
+                id: 'b',
+                answer: 'Dutch & Portuguese'
+            },
+            {
+                id: 'c',
+                answer: 'French & Dutch'
+            },
+            {
+                id: 'd',
+                answer: 'German & Portuguese'
+            }
+        ],
+        correct_answer: 'd'
+    },
+    {
+        question: 'What did I want to be when I grew up?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'Doctor'
+            },
+            {
+                id: 'b',
+                answer: 'Lawyer'
+            },
+            {
+                id: 'c',
+                answer: 'Accountant'
+            },
+            {
+                id: 'd',
+                answer: 'Professor'
+            }
+        ],
+        correct_answer: 'b'
+    },
+    {
+        question: 'To which of these have I not travelled?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'Russia'
+            },
+            {
+                id: 'b',
+                answer: 'Greece'
+            },
+            {
+                id: 'c',
+                answer: 'Turkey'
+            },
+            {
+                id: 'd',
+                answer: 'Italy'
+            }
+        ],
+        correct_answer: 'c'
+    },
+    {
+        question: 'What is my favourite flavour of ice cream?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'Banana'
+            },
+            {
+                id: 'b',
+                answer: 'Coffee'
+            },
+            {
+                id: 'c',
+                answer: 'Strawberry'
+            },
+            {
+                id: 'd',
+                answer: 'Mint Chocolate Chip'
+            }
+        ],
+        correct_answer: 'b'
+    },
+    {
+        question: 'Which of the following would I most like to keep as a pet?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'Rats'
+            },
+            {
+                id: 'b',
+                answer: 'Fish'
+            },
+            {
+                id: 'c',
+                answer: 'Lizards'
+            },
+            {
+                id: 'd',
+                answer: 'Snakes'
+            }
+        ],
+        correct_answer: 'a'
+    },
+    {
+        question: 'What is my favourite season of the year?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'Winter'
+            },
+            {
+                id: 'b',
+                answer: 'Spring'
+            },
+            {
+                id: 'c',
+                answer: 'Summer'
+            },
+            {
+                id: 'd',
+                answer: 'Fall'
+            }
+        ],
+        correct_answer: 'c'
+    }
+];
